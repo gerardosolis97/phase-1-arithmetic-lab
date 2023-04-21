c054aae33df180c1e773e5a22b788b2290e5af65
Gerardo Solis <gerardosolis327@gmail.com>
2023-04-20 21:13:03 -0500

    lab completed

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1
+
+let num3 = 9
+let num4 = 5
+let mod = num3 % num4
+
+let max = Math.max(15, 16, 17, 18, 19, 20)
